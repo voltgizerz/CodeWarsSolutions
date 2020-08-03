a3931312f531ee418b8869bb8e1a4dbaee7cf407
Felix Fernando <54938467+voltgizerz@users.noreply.github.com>
2020-08-03 20:48:04 +0700

    ✨ Added 2 New Problems

---
+++ 8 kyu - Beginner/8 kyu - Grasshopper - Debug sayHello/solve.java
@@ -0,0 +1,11 @@
+public class GrassHopper {
+
+    public static String sayHello(String name) {
+
+        return "Hello, "+name;
+    }
+}
+
+// python 
+// def say_hello(name):
+//     return "Hello, "+name
